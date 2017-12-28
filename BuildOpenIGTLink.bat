16913af6d5c15e9fed433c3db2c65a37aa4efc7e
Adam Rankin <adam.rankin@gmail.com>
2017-12-28 13:43:43 -0500

    Adding convenience batch file to configure and build OpenIGTLink
    Assumes cmake installed and in path

---
+++ BuildOpenIGTLink.bat
@@ -0,0 +1,17 @@
+@ECHO off
+
+REM Build 32bit debug and release
+mkdir .\UWPOpenIGTLink\OpenIGTLink-bin-Win32
+pushd .\UWPOpenIGTLink\OpenIGTLink-bin-Win32
+cmake -G "Visual Studio 15 2017" ..\OpenIGTLink -DBUILD_TESTING:BOOL=OFF
+cmake --build . --config Release
+cmake --build . --config Debug
+popd
+
+REM Build 32bit debug and release
+mkdir .\UWPOpenIGTLink\OpenIGTLink-bin-x64
+pushd .\UWPOpenIGTLink\OpenIGTLink-bin-x64
+cmake -G "Visual Studio 15 2017 Win64" ..\OpenIGTLink -DBUILD_TESTING:BOOL=OFF
+cmake --build . --config Release
+cmake --build . --config Debug
+popd
